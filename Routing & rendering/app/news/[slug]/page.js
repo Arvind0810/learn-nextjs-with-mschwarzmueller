69aaa463b816d8baa1534aafda6a1591826cd6c1
Arvind Sharma <arvind.sharma@1finance.org.in>
2024-08-22 00:00:36 +0530

    setting up new News project and configuring it's pages and components and Not found states

---
+++ Routing & rendering/app/news/[slug]/page.js
@@ -0,0 +1,18 @@
+import { DUMMY_NEWS } from "@/dummy-news"
+import { notFound } from "next/navigation"
+
+export default function DetailPage({params}){
+    const {slug} = params
+    const news = DUMMY_NEWS.find((news) => news.slug === slug)
+    if(!news) notFound()
+    return (
+        <article className="news-article">
+            <header>
+                <img src={`/images/news/${news.image}`} alt={news.title} />
+                <h1>{news.title}</h1>
+                <time dateTime={news.date} >{news.date}</time>
+            </header>
+            <p>{news.content}</p>
+        </article>
+    )
+}
