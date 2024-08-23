bf78ece50550d88e8cb02dda8025ca3da0ca477c
Arvind Sharma <arvind.sharma@1finance.org.in>
2024-08-23 13:12:52 +0530

    Navigating programmatically

--- Routing & rendering/app/news/[slug]/@modal/(.)image/page.js
+++ Routing & rendering/app/news/[slug]/@modal/(.)image/page.js
@@ -1,7 +1,10 @@
+'use client'
+
 import { DUMMY_NEWS } from "@/dummy-news"
-import { notFound } from "next/navigation"
+import { notFound, useRouter } from "next/navigation"
 
 export default function ImagePreview({params}){
+    const router = useRouter()
     const {slug} = params
     const newsItem = DUMMY_NEWS.find((news) => news.slug === slug)
     if(!newsItem){
@@ -9,7 +12,7 @@ export default function ImagePreview({params}){
     }
 
     return <>
-    <div className="modal-backdrop" >
+    <div className="modal-backdrop" onClick={router.back} >
     <dailog className="modal" open >
         <div className="fullscreen-image" >
             <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} style={{maxWidth:'100%'}} />
