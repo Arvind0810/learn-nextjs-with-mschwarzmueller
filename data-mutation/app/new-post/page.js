7bd55517e99335b5169d4f54ad2f27c123ec7bee
Arvind Sharma <arsharma1996.as@gmail.com>
2024-08-26 18:09:53 +0530

    project finished data mutation

--- data-mutation/app/new-post/page.js
+++ data-mutation/app/new-post/page.js
@@ -1,41 +1,6 @@
 import PostForm from '@/components/post-form';
-import { storePost } from '@/lib/posts';
-import { redirect } from 'next/navigation';
-
+import { createPost } from '@/actions/post';
 export default function NewPostPage() {
-
-  async function createPost(prevState,formData) {
-    "use server";
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
-
-    let error = []
-
-    if(!title || title.trim().length === 0){
-      error.push('Title is required.')
-    }
-
-    if(!content || content.trim().length === 0) {
-      error.push('Content is required.')
-    }
-
-    if(!image || image.size === 0){
-      error.push('Image is required.')
-    }
-
-    if(error.length > 0){
-      return {error}
-    }
-
-    storePost({
-      imageUrl: '',
-      title,
-      content,
-      userId: 1
-    })
-    redirect('/feed')
-  }
 
   return <>
     <PostForm action={createPost} />
