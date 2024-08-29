fc411628e69dfca1eccde51c5391cb972fa667d9
Arvind Sharma <arsharma1996.as@gmail.com>
2024-08-29 23:50:56 +0530

    verifying session and protecting routes against unauthorized access

--- authentication/lib/auth.js
+++ authentication/lib/auth.js
@@ -1,9 +1,10 @@
 import { Lucia } from "lucia";
 import { BetterSqlite3Adapter } from "@lucia-auth/adapter-sqlite";
 import db from "./db";
+import { cookies } from "next/headers";
 
 const adapter = new BetterSqlite3Adapter(db,{
-    user: 'user',
+    user: 'users',
     session: 'sessions'
 })
 
@@ -15,3 +16,59 @@ const lucia = new Lucia(adapter, {
         }
     }
 })
+
+export async function createAuthSession(userId){
+    const session = await lucia.createSession(userId, {})
+    const sessionCookie = lucia.createSessionCookie(session.id)
+    cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+    )
+}
+
+export async function verifyAuth(){
+    const sessionCookie = cookies().get(lucia.sessionCookieName)
+
+    if(!sessionCookie){
+        return {
+            user: null,
+            session: null
+        }
+    }
+
+    const sessionId = sessionCookie.value
+
+    if(!sessionId){
+        return {
+            user: null,
+            session: null
+        }
+    }
+    
+    const result = await lucia.validateSession(sessionId);
+    
+    try {
+        if(result.session && result.session.fresh){
+            const sessionCookie = lucia.createSessionCookie(result.session.id)
+            cookies().set(
+                sessionCookie.name,
+                sessionCookie.value,
+                sessionCookie.attributes
+            )
+        }
+
+        if(!result.session){
+            const sessionCookie = lucia.createSessionCookie()
+            cookies().set(
+                sessionCookie.name,
+                sessionCookie.value,
+                sessionCookie.attributes
+            )
+        }
+    } catch (error) {
+        
+    }
+
+    return result
+}
