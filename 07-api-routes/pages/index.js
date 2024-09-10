d0e9331488a5c727ea95df48fefb376b71aa14fb
Arvind Sharma <arsharma1996.as@gmail.com>
2024-09-10 23:17:22 +0530

    working with next api and sending request to api routes

--- 07-api-routes/pages/index.js
+++ 07-api-routes/pages/index.js
@@ -1,7 +1,43 @@
+import { useRef } from 'react'
+
 function HomePage() {
+  const emailRef = useRef()
+  const feedbackRef = useRef()
+  
+  function submitFormHandler(e){
+    e.preventDefault()
+    const enteredEmail = emailRef.current.value
+    const enteredFeedback = feedbackRef.current.value
+
+    const reqBody = {
+      email: enteredEmail,
+      text: enteredFeedback
+    }
+    
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(reqBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((res) => res.json())
+    .then((data) => console.log(data))
+  }
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email" >Your Email Address</label>
+          <input type="email" id="email" ref={emailRef} />
+        </div>
+
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" row="5" ref={feedbackRef} ></textarea>
+        </div>
+        <button type="submit">Send Feedback</button>
+      </form>
     </div>
   );
 }
